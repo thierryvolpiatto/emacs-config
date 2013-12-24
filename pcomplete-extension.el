dd5452654325a244bf0524fdc675a8db6f9bfd99
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2013-12-24 09:44:08 +0100

    * pcomplete-extension.el: Remove commented code now unused.

--- pcomplete-extension.el
+++ pcomplete-extension.el
@@ -147,21 +147,6 @@
 
 ;;; Redefine emacs functions to have completion after sudo
 ;;
-;; (defun shell-command-completion ()
-;;   "Return the completion data for the command at point, if any."
-;;   (let ((filename (comint-match-partial-filename))
-;;         (args (split-string
-;;                (buffer-substring-no-properties
-;;                 (point) (save-excursion (shell-backward-command 1) (point))))))
-;;     (when (and filename
-;;                (save-match-data (not (string-match "[~/]" filename)))
-;;                (or (and (string= (car args) "sudo")
-;;                         (string= filename (cadr args)))
-;;                    (and (not (string= (car args) "sudo"))
-;;                         (eq (match-beginning 0)
-;;                             (save-excursion (shell-backward-command 1) (point))))))
-;;       (shell--command-completion-data))))
-
 (defun shell-backward-command (&optional arg)
   "Move backward across ARG shell command(s).  Does not cross lines.
 See `shell-command-regexp'."
