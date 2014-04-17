86cc86d30618987a1e0989ff5f8adcb52a924381
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-04-17 18:31:17 +0200

    * tv-utils.el (tv/define-key-with-subkeys): Simplify, remove last let.

--- tv-utils.el
+++ tv-utils.el
@@ -941,18 +941,18 @@ Any other keys pressed run their assigned command defined in MAP and exit the lo
        #'(lambda ()
            (interactive)
            (call-interactively ,command)
-           (while (let ((input (read-key))) 
+           (while (let ((input (read-key)) kb com) 
                     (cl-case input
                       (,subkey (call-interactively ,command) t)
                       ,@other-keys
-                      (t (let* ((kb (this-command-keys-vector))
-                                (com (lookup-key ,map kb)))
-                           (if (commandp com)
-                               (call-interactively com)
-                             (setq unread-command-events
-                                   (nconc (mapcar 'identity
-                                                  (this-single-command-raw-keys))
-                                          unread-command-events))))
+                      (t (setq kb  (this-command-keys-vector))
+                         (setq com (lookup-key ,map kb))
+                         (if (commandp com)
+                             (call-interactively com)
+                           (setq unread-command-events
+                                 (nconc (mapcar 'identity
+                                                (this-single-command-raw-keys))
+                                        unread-command-events)))
                          nil))))))))
 
 
