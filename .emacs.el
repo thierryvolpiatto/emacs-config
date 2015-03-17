935cccd74788f05664b94b2f7c387dc40bd91aa8
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-03-17 12:15:05 +0100

    * .emacs.el: Remove now unused winner code. * init-helm-thierry.el: Add helm-handle-winner-boring-buffers to cleanup-hook.

--- .emacs.el
+++ .emacs.el
@@ -1721,50 +1721,6 @@ In Transient Mark mode, activate mark if optional third arg ACTIVATE non-nil."
                               "*esh command on file*"
                               ))
 
-(when (tv-require 'winner)
-  (defvar winner-boring-buffers-regexp "\\*[hH]elm.*")
-  (defun winner-set1 (conf)
-    ;; For the format of `conf', see `winner-conf'.
-    (let* ((buffers nil)
-           (alive
-            ;; Possibly update `winner-point-alist'
-            (cl-loop for buf in (mapcar 'cdr (cdr conf))
-                     for pos = (winner-get-point buf nil)
-                     if (and pos (not (memq buf buffers)))
-                     do (push buf buffers)
-                     collect pos)))
-      (winner-set-conf (car conf))
-      (let (xwins)                      ; to be deleted
-
-        ;; Restore points
-        (dolist (win (winner-sorted-window-list))
-          (unless (and (pop alive)
-                       (setf (window-point win)
-                             (winner-get-point (window-buffer win) win))
-                       (not (or (member (buffer-name (window-buffer win))
-                                        winner-boring-buffers)
-                                (string-match winner-boring-buffers-regexp
-                                              (buffer-name (window-buffer win))))))
-            (push win xwins)))          ; delete this window
-
-        ;; Restore marks
-        (letf (((current-buffer)))
-              (cl-loop for buf in buffers
-                       for entry = (cadr (assq buf winner-point-alist))
-                       for win-ac-reg = (winner-active-region)
-                       do (progn (set-buffer buf)
-                                 (set-mark (car entry))
-                                 (setf win-ac-reg (cdr entry)))))
-        ;; Delete windows, whose buffers are dead or boring.
-        ;; Return t if this is still a possible configuration.
-        (or (null xwins)
-            (progn
-              (mapc 'delete-window (cdr xwins)) ; delete all but one
-              (unless (one-window-p t)
-                (delete-window (car xwins))
-                t))))))
-
-  (defalias 'winner-set 'winner-set1))
 (winner-mode 1)
 
 ;;; Battery
