559be5e5d98c1f61bf4678abc18ffc4179be1964
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-05-19 08:05:17 +0200

    * init-helm-thierry.el: Remove old commented code.

--- init-helm-thierry.el
+++ init-helm-thierry.el
@@ -31,27 +31,6 @@
   (shell-command-to-string
    "git log --pretty='format:%H' -1"))
 
-;; (defun helm-ff-goto-line (candidate)
-;;   (let ((linum (let ((str (with-helm-current-buffer
-;;                             (buffer-substring-no-properties
-;;                              (point-at-bol) (point-at-eol)))))
-;;                  (when (string-match ":\\([0-9]+\\)\\'" str)
-;;                    (match-string 1 str)))))
-;;     (find-file candidate)
-;;     (and linum (not (string= linum ""))
-;;          (helm-goto-line (string-to-number linum) t))))
-
-;; (defmethod helm-setup-user-source :after ((source helm-source-ffiles))
-;;   (helm-source-add-action-to-source-if
-;;    "Find file to line" 'helm-ff-goto-line source
-;;    (lambda (_candidate)
-;;      (let ((str (with-helm-current-buffer
-;;                   (buffer-substring-no-properties
-;;                    (point-at-bol) (point-at-eol)))))
-;;        (and (not (string-match-p ffap-url-regexp str))
-;;             (string-match-p ":\\([0-9]+\\)\\'" str))))
-;;    0))
-
 
 ;;; Helm-command-map
 ;;
