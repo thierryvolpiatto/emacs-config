f1a48477f7094bc758b1a7d381cf92ee03bd0912
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-05-25 08:46:26 +0200

    No need to let-bound coding-system-for-read in packages.
    * package-24.el (package--read-archive-file): Do it.

--- package-24.el
+++ package-24.el
@@ -1134,8 +1134,7 @@ Will throw an error if the archive version is too new."
   (let ((filename (expand-file-name file package-user-dir)))
     (when (file-exists-p filename)
       (with-temp-buffer
-        (let ((coding-system-for-read 'utf-8))
-          (insert-file-contents filename))
+        (insert-file-contents filename)
         (let ((contents (read (current-buffer))))
           (if (> (car contents) package-archive-version)
               (error "Package archive version %d is higher than %d"
