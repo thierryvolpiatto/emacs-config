093b109b8bd519581d8d1dc78175d403b80fa7a8
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-06-14 08:21:15 +0200

    Remove push-mark advice in .emacs.
    * .emacs.el: Remove push-mark advice.
    * init-helm-thierry.el: Enable helm-push-mark-mode.

--- .emacs.el
+++ .emacs.el
@@ -1653,39 +1653,6 @@ only one line entries are supported."
 (setq vc-handled-backends '(Git Hg RCS))
 
 
-;;; Mark-ring
-;;
-;; Redefine push-mark to update mark in global-mark-ring
-(defun tv/push-mark (&optional location nomsg activate)
-  (unless (null (mark t))
-    (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
-    (when (> (length mark-ring) mark-ring-max)
-      (move-marker (car (nthcdr mark-ring-max mark-ring)) nil)
-      (setcdr (nthcdr (1- mark-ring-max) mark-ring) nil)))
-  (set-marker (mark-marker) (or location (point)) (current-buffer))
-  ;; Now push the mark on the global mark ring.
-  (setq global-mark-ring (cons (copy-marker (mark-marker))
-                               ;; Avoid having multiple entries
-                               ;; for same buffer in `global-mark-ring'.
-                               (cl-loop with mb = (current-buffer)
-                                        for m in global-mark-ring
-                                        for nmb = (marker-buffer m)
-                                        unless (eq mb nmb)
-                                        collect m)))
-  (when (> (length global-mark-ring) global-mark-ring-max)
-    (move-marker (car (nthcdr global-mark-ring-max global-mark-ring)) nil)
-    (setcdr (nthcdr (1- global-mark-ring-max) global-mark-ring) nil))
-  (or nomsg executing-kbd-macro (> (minibuffer-depth) 0)
-      (message "Mark set"))
-  (when (or activate (not transient-mark-mode))
-    (set-mark (mark t)))
-  nil)
-
-(if (fboundp 'advice-add)
-    (advice-add 'push-mark :override #'tv/push-mark)
-    (fset 'push-mark 'tv/push-mark))
-
-
 ;;; winner-mode config
 ;;
 ;;
