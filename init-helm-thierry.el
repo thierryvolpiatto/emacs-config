feaf2b24cdcf874b4a869f99b867f8ee5d6cee37
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-09-03 20:35:56 +0200

    Remove all grep cmds.

--- init-helm-thierry.el
+++ init-helm-thierry.el
@@ -150,12 +150,6 @@ First call indent, second complete symbol, third complete fname."
       helm-ff-auto-update-initial-value          t
       helm-grep-default-command                  "ack-grep -Hn --smart-case --no-group %e %p %f"
       helm-grep-default-recurse-command          "ack-grep -H --smart-case --no-group %e %p %f"
-      helm-grep-ag-command                       "ag --line-numbers -S --hidden --color --nogroup %s %s"
-      ;; Allow skipping unwanted files specified in ~/.gitignore_global
-      ;; Added in my .gitconfig with "git config --global core.excludesfile ~/.gitignore_global"
-      helm-ls-git-grep-command                   "git grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p %f"
-      helm-default-zgrep-command                 "zgrep --color=always -a -n%cH -e %p %f"
-      ;; helm-pdfgrep-default-command               "pdfgrep --color always -niH %s %s"
       helm-reuse-last-window-split-state         t
       helm-always-two-windows                    t
       helm-buffers-favorite-modes                (append helm-buffers-favorite-modes
