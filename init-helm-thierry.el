5837e0357a388e1ab2c66952a0852ae1fdb0b6c1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-01-19 20:04:28 +0100

    Rename helm-highlight-number-lines-around-point.

--- init-helm-thierry.el
+++ init-helm-thierry.el
@@ -188,49 +188,51 @@ First call indent, second complete symbol, third complete fname."
 ;;; Helm-variables
 ;;
 ;;
-(setq helm-quick-update                          nil
-      helm-net-prefer-curl                       t
-      helm-kill-ring-threshold                   1
-      helm-raise-command                         "wmctrl -xa %s"
-      helm-scroll-amount                         4
-      helm-idle-delay                            0.01
-      helm-input-idle-delay                      0.01
-      helm-default-external-file-browser         "thunar"
-      helm-pdfgrep-default-read-command          "evince --page-label=%p '%f'"
-      helm-ff-auto-update-initial-value          t
-      helm-grep-default-command                  "ack-grep -Hn --smart-case --no-group %e %p %f"
-      helm-grep-default-recurse-command          "ack-grep -H --smart-case --no-group %e %p %f"
-      helm-reuse-last-window-split-state         t
-      helm-always-two-windows                    t
-      helm-split-window-in-side-p                nil
-      helm-buffers-favorite-modes                (append helm-buffers-favorite-modes
-                                                         '(picture-mode artist-mode))
-      helm-ls-git-status-command                 'magit-status-internal
-      helm-M-x-requires-pattern                  0
-      helm-dabbrev-cycle-threshold               5
-      helm-surfraw-duckduckgo-url                "https://duckduckgo.com/?q=%s&ke=-1&kf=fw&kl=fr-fr&kr=b&k1=-1&k4=-1"
-      helm-boring-file-regexp-list               '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$")
-      helm-buffer-skip-remote-checking           t
-      helm-apropos-fuzzy-match                   t
-      helm-M-x-fuzzy-match                       t
-      helm-lisp-fuzzy-completion                 t
-      helm-completion-in-region-fuzzy-match      t
-      helm-move-to-line-cycle-in-source          t
-      ido-use-virtual-buffers                    t             ; Needed in helm-buffers-list
-      helm-tramp-verbose                         6
-      helm-buffers-fuzzy-matching                t
-      helm-locate-command                        "locate %s -e -A --regex %s"
-      helm-org-headings-fontify                  t
-      helm-autoresize-max-height                 80 ; it is %.
-      helm-autoresize-min-height                 20 ; it is %.
-      helm-buffers-to-resize-on-pa               '("*helm apropos*" "*helm ack-grep*"
-                                                   "*helm grep*" "*helm occur*" "*helm ag*"
-                                                   "*helm multi occur*" "*helm git-grep*"
-                                                   "*helm imenu*" "*helm imenu all*"
-                                                   "*helm gid*" "*helm semantic/imenu*")
-      fit-window-to-buffer-horizontally          1
-      helm-open-github-closed-issue-since        7
-      helm-highlight-number-lines-around-point   30
+(setq helm-quick-update                               nil
+      helm-net-prefer-curl                            t
+      helm-kill-ring-threshold                        1
+      helm-raise-command                              "wmctrl -xa %s"
+      helm-scroll-amount                              4
+      helm-idle-delay                                 0.01
+      helm-input-idle-delay                           0.01
+      helm-default-external-file-browser              "thunar"
+      helm-pdfgrep-default-read-command               "evince --page-label=%p '%f'"
+      helm-ff-auto-update-initial-value               t
+      helm-grep-default-command                       "ack-grep -Hn --smart-case --no-group %e %p %f"
+      helm-grep-default-recurse-command               "ack-grep -H --smart-case --no-group %e %p %f"
+      helm-reuse-last-window-split-state              t
+      helm-always-two-windows                         t
+      helm-split-window-in-side-p                     nil
+      helm-buffers-favorite-modes                     (append helm-buffers-favorite-modes
+                                                              '(picture-mode artist-mode))
+      helm-ls-git-status-command                      'magit-status-internal
+      helm-M-x-requires-pattern                       0
+      helm-dabbrev-cycle-threshold                    5
+      helm-surfraw-duckduckgo-url
+      "https://duckduckgo.com/?q=%s&ke=-1&kf=fw&kl=fr-fr&kr=b&k1=-1&k4=-1"
+      helm-boring-file-regexp-list
+      '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$")
+      helm-buffer-skip-remote-checking                t
+      helm-apropos-fuzzy-match                        t
+      helm-M-x-fuzzy-match                            t
+      helm-lisp-fuzzy-completion                      t
+      helm-completion-in-region-fuzzy-match           t
+      helm-move-to-line-cycle-in-source               t
+      ido-use-virtual-buffers                         t             ; Needed in helm-buffers-list
+      helm-tramp-verbose                              6
+      helm-buffers-fuzzy-matching                     t
+      helm-locate-command                             "locate %s -e -A --regex %s"
+      helm-org-headings-fontify                       t
+      helm-autoresize-max-height                      80 ; it is %.
+      helm-autoresize-min-height                      20 ; it is %.
+      helm-buffers-to-resize-on-pa                    '("*helm apropos*" "*helm ack-grep*"
+                                                        "*helm grep*" "*helm occur*" "*helm ag*"
+                                                        "*helm multi occur*" "*helm git-grep*"
+                                                        "*helm imenu*" "*helm imenu all*"
+                                                        "*helm gid*" "*helm semantic/imenu*")
+      fit-window-to-buffer-horizontally               1
+      helm-open-github-closed-issue-since             7
+      helm-highlight-matches-around-point-max-lines   30
       helm-search-suggest-action-wikipedia-url
       "https://fr.wikipedia.org/wiki/Special:Search?search=%s"
       helm-wikipedia-suggest-url
