ee4c0664409ef33cc24da7d3d26099091dcf9634
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-02-05 18:55:42 +0100

    Use require for w3m.

--- .emacs.el
+++ .emacs.el
@@ -1337,7 +1337,7 @@ in this cl-case start Gnus plugged, otherwise start it unplugged."
 ;;; W3m
 ;;
 (use-package w3m
-    :init (load "config-w3m")
+    :init (require 'config-w3m)
     :bind ("<f7> h" . w3m)
     :defer t)
 
