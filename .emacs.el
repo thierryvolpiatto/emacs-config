22b70604504f6deba11a7d2e6d8b75fe0f50f906
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-01-25 09:06:31 +0100

    Make adaptive-fill-mode local in git-commit buffers.

--- .emacs.el
+++ .emacs.el
@@ -925,6 +925,7 @@ If your system's ping continues until interrupted, you can try setting
     (bind-key "C"    'magit-commit-add-log magit-diff-mode-map)
     (bind-key "C-]"  'magit-toggle-margin magit-log-mode-map)
     (bind-key "<f2>" 'magit-status)
+    (add-hook 'git-commit-setup-hook (lambda () (setq-local adaptive-fill-mode nil)))
     :no-require t)
 
 ;;; Emamux
