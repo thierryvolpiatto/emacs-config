77718a0dceca8258bbb754a66d738752f0b8e4d6
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-10-27 07:07:14 +0200

    Fix push-mark to avoid dups in mark-ring

--- .emacs.el
+++ .emacs.el
@@ -226,9 +226,11 @@ This allow installation of org from melpa when :ensure is specified."
 ;;  position.
 (defun tv/advice-push-mark (&optional location nomsg activate)
   (unless (null (mark t))
-    (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
+    (let ((marker (copy-marker (mark-marker))))
+      (setq mark-ring (cons marker (delete marker mark-ring))))
     (when (> (length mark-ring) mark-ring-max)
-      (move-marker (car (nthcdr mark-ring-max mark-ring)) nil)
+      ;; Move marker to nowhere.
+      (set-marker (car (nthcdr mark-ring-max mark-ring)) nil)
       (setcdr (nthcdr (1- mark-ring-max) mark-ring) nil)))
   (set-marker (mark-marker) (or location (point)) (current-buffer))
   ;; Now push the mark on the global mark ring.
@@ -241,7 +243,7 @@ This allow installation of org from melpa when :ensure is specified."
                                         unless (eq mb nmb)
                                         collect m)))
   (when (> (length global-mark-ring) global-mark-ring-max)
-    (move-marker (car (nthcdr global-mark-ring-max global-mark-ring)) nil)
+    (set-marker (car (nthcdr global-mark-ring-max global-mark-ring)) nil)
     (setcdr (nthcdr (1- global-mark-ring-max) global-mark-ring) nil))
   (or nomsg executing-kbd-macro (> (minibuffer-depth) 0)
       (message "Mark set"))
