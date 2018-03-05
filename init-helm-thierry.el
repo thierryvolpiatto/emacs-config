e01f1085038f370f4cadda6262607ba2c7a63269
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-03-05 12:04:16 +0100

    Add eshell prompts to helm-commands-using-frame

--- init-helm-thierry.el
+++ init-helm-thierry.el
@@ -238,7 +238,7 @@ First call indent, second complete symbol, third complete fname."
       helm-always-two-windows                         t
       helm-split-window-inside-p                      nil
       helm-show-completion-display-function           #'helm-display-buffer-in-own-frame
-      helm-commands-using-frame                       '(completion-at-point helm-apropos)
+      helm-commands-using-frame                       '(completion-at-point helm-apropos helm-eshell-prompts)
       helm-actions-inherit-frame-settings             t
       helm-show-action-window-other-window            'left
       helm-buffers-favorite-modes                     (append helm-buffers-favorite-modes '(picture-mode artist-mode))
