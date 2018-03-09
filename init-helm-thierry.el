86ea516d5cd31dc97214d5fe4a75d17773c16722
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-03-09 11:55:34 +0100

    Update helm config

--- init-helm-thierry.el
+++ init-helm-thierry.el
@@ -240,6 +240,7 @@ First call indent, second complete symbol, third complete fname."
       helm-show-completion-display-function           #'helm-display-buffer-in-own-frame
       helm-commands-using-frame                       '(completion-at-point helm-apropos helm-eshell-prompts)
       helm-actions-inherit-frame-settings             t
+      helm-use-frame-when-more-than-two-windows       t
       helm-show-action-window-other-window            'left
       helm-buffers-favorite-modes                     (append helm-buffers-favorite-modes '(picture-mode artist-mode))
       helm-ls-git-status-command                      'magit-status-internal
