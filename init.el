eb2b170d3c7e8026c2297dc118cd442819d27dd4
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-07-19 08:31:07 +0200

    Fix again signature

--- init.el
+++ init.el
@@ -1216,6 +1216,10 @@ are returned unchanged."
         if isinstance(obj, str_type):
             obj = eval(obj, globals())
         doc = inspect.getdoc(obj)
+        if callable(obj):
+            sig = inspect.formatargspec(*argspec_function(obj))
+        else:
+            sig = None
         if not doc and callable(obj):
             target = None
             if inspect.isclass(obj) and hasattr(obj, '__init__'):
@@ -1232,7 +1236,6 @@ are returned unchanged."
                 )
         else:
             doc = doc.splitlines()[0]
-            sig = inspect.formatargspec(argspec_function(obj)[0])
     except:
         doc = ''
         sig = ''
