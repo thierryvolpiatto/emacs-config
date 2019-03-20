3a11816965a0731105eeebd9b7da9409f9337897
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2019-03-20 11:54:08 +0100

    Reenable w3m config everywhere

--- mu4e-config.el
+++ mu4e-config.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (require 'mu4e-contrib)
+(require 'config-w3m)
 (require 'mu4e-patch)
 
 ;;; Message and smtp settings
@@ -117,8 +118,12 @@
 
 ;;; Html rendering
 (setq mu4e-view-prefer-html t)
-(setq mu4e-html2text-command 'mu4e-shr2text)
-(setq shr-color-visible-luminance-min 75)
+(setq mu4e-html2text-command (cond ((fboundp 'w3m)
+                                    (lambda ()          ; Use emacs-w3m
+                                      (w3m-region (point-min) (point-max))))
+                                   ((executable-find "w3m")
+                                    "w3m -T text/html") ; Use w3m shell-command
+                                   (t 'html2text)))
 
 (setq mail-user-agent      'mu4e-user-agent
       read-mail-command    'mu4e
@@ -237,6 +242,62 @@ AND NOT maildir:/Zoho/Spam AND NOT maildir:/Yahoo/Bulk\\ Mail" "Messages with im
       (save-excursion (insert contents))
       (diff-mode)
       (view-mode))))
+
+(defun tv/mu4e-browse-url-or-show-patch ()
+  (interactive)
+  (require 'helm-net)
+  (let ((url (w3m-active-region-or-url-at-point)))
+    (if (string-match "\\.\\(patch\\|diff\\)\\'" url)
+        (tv/mu4e-show-patch-other-frame url)
+      (browse-url url))))
+(define-key mu4e-view-mode-map (kbd "C-c C-c") 'tv/mu4e-browse-url-or-show-patch)
+
+(defadvice w3m-goto-next-anchor (before go-to-end-of-anchor activate)
+  (when (w3m-anchor-sequence)
+    (let ((pos (next-single-property-change
+                (point) 'w3m-anchor-sequence)))
+      (and pos (goto-char pos)))))
+
+(defadvice w3m-goto-previous-anchor (before go-to-end-of-anchor activate)
+  (when (w3m-anchor-sequence)
+    (let ((pos (previous-single-property-change
+                (point) 'w3m-anchor-sequence)))
+      (and pos (goto-char pos)))))
+
+(defun tv/w3m-next-anchor ()
+  (interactive)
+  (or (stringp (w3m-next-anchor))
+      (let ((pos (point)))
+        (when (eq (get-text-property (point) 'face)
+                  'mu4e-link-face)
+          (setq pos (next-single-property-change (point) 'face)))
+        (let ((next-url (and pos
+                             (or (text-property-any
+                                  pos (point-max)
+                                  'face 'mu4e-link-face)
+                                 (text-property-any
+                                  (point-min) (point-max)
+                                  'face 'mu4e-link-face)))))
+          (and next-url (goto-char next-url))))))
+
+(defun tv/w3m-previous-anchor ()
+  (interactive)
+  (require 'helm-lib)
+  (or (stringp (w3m-previous-anchor))
+      (let ((prev-url (save-excursion
+                        (helm-awhile (re-search-backward
+                                      ffap-url-regexp nil t)
+                          (goto-char (match-beginning 0))
+                          (when (eq (get-text-property
+                                     (point) 'face)
+                                    'mu4e-link-face)
+                            (cl-return (point)))
+                          (goto-char it)))))
+        (and prev-url (goto-char prev-url)))))
+
+(define-key mu4e-view-mode-map (kbd "<C-tab>")   'tv/w3m-next-anchor)
+(define-key mu4e-view-mode-map (kbd "<backtab>") 'tv/w3m-previous-anchor)
+(define-key mu4e-view-mode-map (kbd "X") 'mu4e-view-save-attachment-multi)
 
 ;;; A simplified and more efficient version of `article-translate-strings'.
 ;;
