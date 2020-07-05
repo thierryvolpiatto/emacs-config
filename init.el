a83670451193403a08fa241b2c6b61bd03aaaa2e
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2020-07-05 12:18:43 +0200

    Use upstream iedit now my PR have been merged

--- init.el
+++ init.el
@@ -1174,10 +1174,7 @@ If your system's ping continues until interrupted, you can try setting
 ;;; Iedit
 ;;
 (use-package iedit
-  :straight (iedit :host github :repo "victorhge/iedit"
-                   :fork (:host github
-                          :repo "thierryvolpiatto/iedit"
-                          :branch "increment_occurences"))
+  :straight (iedit :host github :repo "victorhge/iedit")
   :config
   (defun iedit-narrow-to-defun (arg)
     (interactive "P")
