073b68823a90e9769bd6021783c854725a6e3a62
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2020-07-24 13:58:22 +0200

    Try anaconda for python

--- init.el
+++ init.el
@@ -1417,6 +1417,9 @@ In the absence of INDEX, just call `eldoc-docstring-format-sym-doc'."
 ;;; Python config
 ;;
 ;;
+(use-package anaconda-mode
+  :straight t)
+
 (use-package python
   :no-require t
   :init
@@ -1427,10 +1430,13 @@ In the absence of INDEX, just call `eldoc-docstring-format-sym-doc'."
      python-shell-interpreter-args "-i --autoindent --simple-prompt --InteractiveShell.display_page=True"
      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: ")
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
     (add-hook 'python-mode-hook 'flymake-mode) ;; Needs pyflakes
     (add-hook 'python-mode-hook
               (lambda ()
-                (define-key python-mode-map (kbd "C-m") 'newline-and-indent))))
+                (define-key python-mode-map (kbd "C-m") 'newline-and-indent)
+                (define-key python-mode-map (kbd "C-c '") 'flymake-goto-next-error))))
   :bind ("<f11> p" . python-shell-switch-to-shell))
 
 ;;; Tramp-config
