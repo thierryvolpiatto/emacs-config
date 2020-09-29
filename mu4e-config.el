b98ee76862ba9373ed87de6bb4438ec51ecc52e5
Thierry Volpiatto <thievol@posteo.net>
2020-09-29 21:23:52 +0200

    Enable autocrypt

--- mu4e-config.el
+++ mu4e-config.el
@@ -5,7 +5,16 @@
 (use-package mu4e-contrib)
 (use-package config-w3m)
 (use-package mu4e-patch)
-
+(use-package autocrypt
+  :straight t
+  :hook ((mu4e-main-mode . autocrypt-mode)
+         (gnus-mode . autocrypt-mode)
+         (message-mode . autocrypt-mode))
+  :config
+  (setq autocrypt-accounts
+        '(("thievol@posteo.net"
+          "23DB707D137BAF79E283FC700EC56D141D16EF93"
+          mutual))))
 
 ;;; Message and smtp settings
 
