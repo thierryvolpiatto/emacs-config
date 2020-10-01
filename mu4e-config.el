215d473b60a0d6dbf2f03c296dec91b2ce3d7dd3
Thierry Volpiatto <thievol@posteo.net>
2020-10-01 09:27:45 +0200

    Setup autocrypt account

--- mu4e-config.el
+++ mu4e-config.el
@@ -13,8 +13,8 @@
   :config
   (setq autocrypt-accounts
         '(("thievol@posteo.net"
-          "23DB707D137BAF79E283FC700EC56D141D16EF93"
-          mutual))))
+           "23DB707D137BAF79E283FC700EC56D141D16EF93"
+           mutual))))
 
 ;;; Message and smtp settings
 
