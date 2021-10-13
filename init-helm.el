2fb244826b7c0c8fea101f1b852c366b0e781244
Thierry Volpiatto <thievol@posteo.net>
2021-10-13 13:23:22 +0200

    Update helm config

--- init-helm.el
+++ init-helm.el
@@ -274,7 +274,8 @@
         helm-trash-remote-files                  t
         helm-dwim-target                         'next-window
         helm-locate-recursive-dirs-command "fd --hidden --type d --glob '*%s*' %s"
-        helm-ff-eshell-unwanted-aliases '("sudo" "cdu"))
+        helm-ff-eshell-unwanted-aliases '("sudo" "cdu"
+                                          "gpg-pubkey-export-armor" "gpg-secretkey-export-armor"))
   (customize-set-variable 'helm-ff-lynx-style-map t)
   (define-key helm-read-file-map (kbd "RET") 'helm-ff-RET)
   (define-key helm-find-files-map (kbd "C-i") nil)
