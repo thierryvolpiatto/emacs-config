7ce79a99742db97480ccf8af1a982f5ec3deaa62
Thierry Volpiatto <thievol@posteo.net>
2022-04-05 11:11:16 +0200

    Use display-buffer-alist instead of deprecated special-display-regexps
    helm: use new var helm-persistent-action-use-special-display.

--- init-helm.el
+++ init-helm.el
@@ -14,6 +14,7 @@
                                                     helm-apropos
                                                     helm-eshell-prompts helm-imenu
                                                     helm-imenu-in-all-buffers)
+        helm-persistent-action-use-special-display t
         helm-actions-inherit-frame-settings       t
         helm-use-frame-when-more-than-two-windows t
         helm-use-frame-when-dedicated-window      t
--- init.el
+++ init.el
@@ -780,54 +780,63 @@ With a prefix arg decrease transparency."
                                   )))
 
     ;; Special buffer display.
-    (add-hook 'window-setup-hook
-              (lambda ()
-                (setq special-display-regexps `(("\\*Help"
-                                                 (minibuffer . nil)
-                                                 (width . 80)
-                                                 (height . 24)
-                                                 (left-fringe . 0)
-                                                 (border-width . 0)
-                                                 (menu-bar-lines . 0)
-                                                 (tool-bar-lines . 0)
-                                                 (unsplittable . t)
-                                                 (top . 24)
-                                                 (left . 450)
-                                                 (background-color . "Lightsteelblue1")
-                                                 (foreground-color . "black")
-                                                 (alpha . nil)
-                                                 (fullscreen . nil))
-                                                ("\\*Compile-Log"
-                                                 (minibuffer . nil)
-                                                 (width . 85)
-                                                 (height . 24)
-                                                 (left-fringe . 0)
-                                                 (border-width . 0)
-                                                 (menu-bar-lines . 0)
-                                                 (tool-bar-lines . 0)
-                                                 (unsplittable . t)
-                                                 (top . 24)
-                                                 (left . 450)
-                                                 (background-color . "Brown4")
-                                                 (foreground-color . "black")
-                                                 (alpha . nil)
-                                                 (fullscreen . nil))
-                                                ("\\*helm apt show\\*"
-                                                 (minibuffer . nil)
-                                                 (width . 80)
-                                                 (height . 24)
-                                                 (left-fringe . 0)
-                                                 (border-width . 0)
-                                                 (menu-bar-lines . 0)
-                                                 (tool-bar-lines . 0)
-                                                 (unsplittable . t)
-                                                 (top . 24)
-                                                 (left . 450)
-                                                 (background-color . "Lightsteelblue4")
-                                                 (foreground-color . "black")
-                                                 (alpha . nil)
-                                                 (fullscreen . nil))
-                                                )))))
+    (customize-set-variable 'display-buffer-alist
+                            (append '(("\\*Help"
+                                       (display-buffer-pop-up-frame)
+                                       (pop-up-frame-parameters .
+                                        ((minibuffer . nil)
+                                         (width . 80)
+                                         (height . 24)
+                                         (left-fringe . 0)
+                                         (border-width . 0)
+                                         (menu-bar-lines . 0)
+                                         (tool-bar-lines . 0)
+                                         (unsplittable . t)
+                                         (top . 24)
+                                         (left . 450)
+                                         (background-color . "Lightsteelblue1")
+                                         (foreground-color . "black")
+                                         (alpha . nil)
+                                         (fullscreen . nil))))
+                                      ("\\*Compile-Log"
+                                       (display-buffer-reuse-window
+                                        display-buffer-pop-up-frame)
+                                       (reusable-frames . 0)
+                                       (pop-up-frame-parameters .
+                                        ((minibuffer . nil)
+                                         (width . 85)
+                                         (height . 24)
+                                         (left-fringe . 0)
+                                         (border-width . 0)
+                                         (menu-bar-lines . 0)
+                                         (tool-bar-lines . 0)
+                                         (unsplittable . t)
+                                         (top . 24)
+                                         (left . 450)
+                                         (background-color . "Brown4")
+                                         (foreground-color . "black")
+                                         (alpha . nil)
+                                         (fullscreen . nil))))
+                                      ("\\*helm apt show\\*"
+                                       (display-buffer-pop-up-frame)
+                                       (pop-up-frame-parameters .
+                                        ((minibuffer . nil)
+                                         (width . 80)
+                                         (height . 24)
+                                         (left-fringe . 0)
+                                         (border-width . 0)
+                                         (menu-bar-lines . 0)
+                                         (tool-bar-lines . 0)
+                                         (unsplittable . t)
+                                         (top . 24)
+                                         (left . 450)
+                                         (background-color . "Lightsteelblue4")
+                                         (foreground-color . "black")
+                                         (alpha . nil)
+                                         (fullscreen . nil))))
+                                      ("^\\*osm"
+                                       (display-buffer-same-window)))
+                                    display-buffer-alist)))
   :bind ("C-8" . tv/transparency-modify))
 
 (use-package window
@@ -1968,10 +1977,7 @@ Variable adaptive-fill-mode is disabled when a docstring field is detected."
 
 ;;; Wgrep
 ;;
-(use-package wgrep-helm
-  :ensure t
-  :config
-  (setq wgrep-enable-key "\C-x\C-q"))
+(use-package wgrep-helm :ensure t)
 
 ;;; psession
 ;;
