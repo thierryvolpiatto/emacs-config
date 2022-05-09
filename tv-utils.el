86098117b382253912ece9eb7656729c8caafac6
Thierry Volpiatto <thievol@posteo.net>
2022-05-09 17:40:34 +0200

    Add command and minor-mode to start/stop byzanz

--- tv-utils.el
+++ tv-utils.el
@@ -1688,9 +1688,14 @@ file-local variable.\n")
             (when (looking-at "(") (forward-sexp 1))))
         (goto-char (point-min))
         (indent-region (point-min) (point-max))))))
+
+;;; Record Emacs screencast with byzanz.
+;;
+(defgroup byzanz-record nil "Record screencast."
+          :group 'multimedia)
 
 ;;;###autoload
-(defun tv/byzanz-record (file)
+(defun byzanz-record (file)
   "Record a screencast with byzanz."
   (interactive "FRecord to file: ")
   (let* ((height  (number-to-string (+ (frame-pixel-height)
@@ -1699,13 +1704,34 @@ file-local variable.\n")
          (width   (number-to-string (frame-pixel-width)))
          (process (start-process "byzanz" "*byzanz log*"
                                  "byzanz-record"
-                                 ;; Stop when clicking on window.
-                                 "--exec=xdotool selectwindow" "--delay=5"
-                                 "-w" width "-h" height file)))
+                                 "--exec=sleep 1000000"
+                                 "--delay=5"
+                                 "-c" "-w" width "-h" height file)))
+    (byzanz-record-mode 1)
     (set-process-sentinel
      process (lambda (_proc event)
                (when (string= event "finished\n")
+                 (byzanz-record-mode -1)
                  (message "Screencast recorded to `%s'" file))))))
+
+(defun byzanz-record-stop ()
+  "Stop byzanz recording.
+Don't bind this to global-map but to `byzanz-record-mode-map' instead."
+  (interactive)
+  (call-process "killall" nil nil nil "sleep"))
+
+(defvar byzanz-record-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "S-<f12>") 'byzanz-record-stop)
+    map))
+
+(define-minor-mode byzanz-record-mode
+    "A minor mode to stop byzanz-record."
+  :group 'byzanz-record
+  :global t
+  (message "Byzanz started recording, hit `S-<f12>' to stop"))
+(put 'byzanz-record-mode 'no-helm-mx t)
+
 
 (provide 'tv-utils)
 
