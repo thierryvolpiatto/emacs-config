1203bf6d437b1a119bd036d7b122f79f5d81589d
Thierry Volpiatto <thievol@posteo.net>
2022-10-06 17:12:17 +0200

    Use helm-mark-ring instead of helm-all-mark-rings

--- init-helm.el
+++ init-helm.el
@@ -743,7 +743,7 @@ First call indent, second complete symbol, third complete fname."
 (global-set-key (kbd "M-x")                          'helm-M-x)
 (global-set-key (kbd "M-y")                          'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f")                      'helm-find-files)
-(global-set-key (kbd "C-c <SPC>")                    'helm-all-mark-rings)
+(global-set-key (kbd "C-c <SPC>")                    'helm-mark-ring)
 (global-set-key [remap bookmark-jump]                'helm-filtered-bookmarks)
 (global-set-key (kbd "C-:")                          'helm-eval-expression-with-eldoc)
 (global-set-key (kbd "C-,")                          'helm-calcul-expression)
