9cecdb5fe4ce8ce4802c6f16cafea89736de9194
Thierry Volpiatto <thievol@posteo.net>
2022-11-09 17:31:49 +0100

    Reindent

--- early-init.el
+++ early-init.el
@@ -1,5 +1,5 @@
 ;;; early-init.el --evaluated before init.el
 
-(setq package-archives        '(("melpa" . "https://melpa.org/packages/")
-                                ("gnu"   . "https://elpa.gnu.org/packages/")
-                                ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
