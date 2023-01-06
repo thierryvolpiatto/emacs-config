cc02ae2ab49df83503bae540b0ebac3338d6a755
Thierry Volpiatto <thievol@posteo.net>
2023-01-06 14:00:40 +0100

    Untabify

--- describe-variable.el
+++ describe-variable.el
@@ -13,9 +13,9 @@ It is based on emacs-28.2 version of `describe-variable'.
 Large values are not pretty printed."
   (interactive
    (let ((v (variable-at-point))
-	 (enable-recursive-minibuffers t)
+         (enable-recursive-minibuffers t)
          (orig-buffer (current-buffer))
-	 val)
+         val)
      (setq val (completing-read
                 (format-prompt "Describe variable" (and (symbolp v) v))
                 #'help--symbol-completion-table
@@ -29,37 +29,37 @@ Large values are not pretty printed."
                 t nil nil
                 (if (symbolp v) (symbol-name v))))
      (list (if (equal val "")
-	       v (intern val)))))
+               v (intern val)))))
   (let (file-name)
     (unless (buffer-live-p buffer) (setq buffer (current-buffer)))
     (unless (frame-live-p frame) (setq frame (selected-frame)))
     (if (not (symbolp variable))
-	(user-error "You didn't specify a variable")
+        (user-error "You didn't specify a variable")
       (save-excursion
-	(let ((valvoid (not (with-current-buffer buffer (boundp variable))))
-	      val val-start-pos locus)
-	  ;; Extract the value before setting up the output buffer,
-	  ;; in case `buffer' *is* the output buffer.
-	  (unless valvoid
-	    (with-selected-frame frame
-	      (with-current-buffer buffer
-		(setq val (symbol-value variable)
-		      locus (variable-binding-locus variable)))))
-	  (help-setup-xref (list #'describe-variable variable buffer)
-			   (called-interactively-p 'interactive))
-	  (with-help-window (help-buffer)
-	    (with-current-buffer buffer
-	      (prin1 variable)
-	      (setq file-name (find-lisp-object-file-name variable 'defvar))
+        (let ((valvoid (not (with-current-buffer buffer (boundp variable))))
+              val val-start-pos locus)
+          ;; Extract the value before setting up the output buffer,
+          ;; in case `buffer' *is* the output buffer.
+          (unless valvoid
+            (with-selected-frame frame
+              (with-current-buffer buffer
+                (setq val (symbol-value variable)
+                      locus (variable-binding-locus variable)))))
+          (help-setup-xref (list #'describe-variable variable buffer)
+                           (called-interactively-p 'interactive))
+          (with-help-window (help-buffer)
+            (with-current-buffer buffer
+              (prin1 variable)
+              (setq file-name (find-lisp-object-file-name variable 'defvar))
 
-	      (princ (if file-name
-		         (progn
-		           (princ (format-message
+              (princ (if file-name
+                         (progn
+                           (princ (format-message
                                    " is a variable defined in `%s'.\n\n"
                                    (if (eq file-name 'C-source)
                                        "C source code"
                                      (help-fns-short-filename file-name))))
-		           (with-current-buffer standard-output
+                           (with-current-buffer standard-output
                              (setq help-mode--current-data
                                    (list :symbol variable
                                          :type (if (eq file-name 'C-source)
@@ -67,36 +67,36 @@ Large values are not pretty printed."
                                                  'defvar)
                                          :file file-name))
                              (save-excursion
-			       (re-search-backward (substitute-command-keys
+                               (re-search-backward (substitute-command-keys
                                                     "`\\([^`']+\\)'")
                                                    nil t)
-			       (help-xref-button 1 'help-variable-def
-					         variable file-name)))
-		           (if valvoid
-			       "It is void as a variable."
+                               (help-xref-button 1 'help-variable-def
+                                                 variable file-name)))
+                           (if valvoid
+                               "It is void as a variable."
                              "Its "))
-	               (with-current-buffer standard-output
+                       (with-current-buffer standard-output
                          (setq help-mode--current-data (list :symbol variable
                                                              :type 'variable)))
                        (if valvoid
-		           " is void as a variable."
+                           " is void as a variable."
                          (substitute-command-keys "'s ")))))
-	    (unless valvoid
-	      (with-current-buffer standard-output
-		(setq val-start-pos (point))
-		(princ "value is")
-		(let ((line-beg (line-beginning-position))
-		      (print-rep
-		       (let ((rep
-			      (let ((print-quoted t)
+            (unless valvoid
+              (with-current-buffer standard-output
+                (setq val-start-pos (point))
+                (princ "value is")
+                (let ((line-beg (line-beginning-position))
+                      (print-rep
+                       (let ((rep
+                              (let ((print-quoted t)
                                     (print-circle t))
-				(cl-prin1-to-string val))))
-			 (if (and (symbolp val) (not (booleanp val)))
-			     (format-message "`%s'" rep)
-			   rep))))
-		  (if (< (+ (length print-rep) (point) (- line-beg)) 68)
-		      (insert " " print-rep)
-		    (terpri)
+                                (cl-prin1-to-string val))))
+                         (if (and (symbolp val) (not (booleanp val)))
+                             (format-message "`%s'" rep)
+                           rep))))
+                  (if (< (+ (length print-rep) (point) (- line-beg)) 68)
+                      (insert " " print-rep)
+                    (terpri)
                     (let ((buf (current-buffer)))
                       (with-temp-buffer
                         (lisp-mode-variables nil)
@@ -109,31 +109,31 @@ Large values are not pretty printed."
                             (insert-buffer-substring pp-buffer)))))
                     ;; Remove trailing newline.
                     (and (= (char-before) ?\n) (delete-char -1)))
-		  (let* ((sv (get variable 'standard-value))
-			 (origval (and (consp sv)
-				       (condition-case nil
-					   (eval (car sv) t)
-					 (error :help-eval-error))))
+                  (let* ((sv (get variable 'standard-value))
+                         (origval (and (consp sv)
+                                       (condition-case nil
+                                           (eval (car sv) t)
+                                         (error :help-eval-error))))
                          from)
-		    (when (and (consp sv)
+                    (when (and (consp sv)
                                (not (equal origval val))
                                (not (equal origval :help-eval-error)))
-		      (princ "\nOriginal value was \n")
-		      (setq from (point))
-		      (if (and (symbolp origval) (not (booleanp origval)))
-			  (let* ((rep (cl-prin1-to-string origval))
-				 (print-rep (format-message "`%s'" rep)))
-			    (insert print-rep))
-			(cl-prin1 origval))
+                      (princ "\nOriginal value was \n")
+                      (setq from (point))
+                      (if (and (symbolp origval) (not (booleanp origval)))
+                          (let* ((rep (cl-prin1-to-string origval))
+                                 (print-rep (format-message "`%s'" rep)))
+                            (insert print-rep))
+                        (cl-prin1 origval))
                       (save-restriction
                         (narrow-to-region from (point))
                         (unless (> (- (point) from) describe-variable--offset-value)
                           (save-excursion (pp-buffer))))
-		      (if (< (point) (+ from 20))
-			  (delete-region (1- from) from)))))))
-	    (terpri)
-	    (when locus
-	      (cond
+                      (if (< (point) (+ from 20))
+                          (delete-region (1- from) from)))))))
+            (terpri)
+            (when locus
+              (cond
                ((bufferp locus)
                 (princ (format "Local in buffer %s; "
                                (buffer-name buffer))))
@@ -141,50 +141,50 @@ Large values are not pretty printed."
                 (princ "It is a terminal-local variable; "))
                (t
                 (princ (format "It is local to %S" locus))))
-	      (if (not (default-boundp variable))
-		  (princ "globally void")
-		(let ((global-val (default-value variable)))
-		  (with-current-buffer standard-output
-		    (princ "global value is ")
-		    (if (eq val global-val)
-			(princ "the same.")
-		      (terpri)
+              (if (not (default-boundp variable))
+                  (princ "globally void")
+                (let ((global-val (default-value variable)))
+                  (with-current-buffer standard-output
+                    (princ "global value is ")
+                    (if (eq val global-val)
+                        (princ "the same.")
+                      (terpri)
                       ;; FIXED:
-		      ;; Fixme: pp can take an age if you happen to
-		      ;; ask for a very large expression.  We should
-		      ;; probably print it raw once and check it's a
-		      ;; sensible size before prettyprinting.  -- fx
-		      (let ((from (point)))
+                      ;; Fixme: pp can take an age if you happen to
+                      ;; ask for a very large expression.  We should
+                      ;; probably print it raw once and check it's a
+                      ;; sensible size before prettyprinting.  -- fx
+                      (let ((from (point)))
                         (cl-prin1 global-val)
                         (save-restriction
                           (narrow-to-region from (point))
                           (unless (> (- (point) from) describe-variable--offset-value)
                             (save-excursion (pp-buffer))))
-			;; See previous comment for this function.
-			;; (help-xref-on-pp from (point))
-			(if (< (point) (+ from 20))
-			    (delete-region (1- from) from)))))))
+                        ;; See previous comment for this function.
+                        ;; (help-xref-on-pp from (point))
+                        (if (< (point) (+ from 20))
+                            (delete-region (1- from) from)))))))
               (terpri))
 
-	    ;; If the value is large, move it to the end.
-	    (with-current-buffer standard-output
-	      (when (or (> (- (point-max) (point-min)) describe-variable--offset-value)
+            ;; If the value is large, move it to the end.
+            (with-current-buffer standard-output
+              (when (or (> (- (point-max) (point-min)) describe-variable--offset-value)
                         (> (count-lines (point-min) (point-max)) 10))
-		;; Note that setting the syntax table like below
-		;; makes forward-sexp move over a `'s' at the end
-		;; of a symbol.
-		(set-syntax-table emacs-lisp-mode-syntax-table)
-		(goto-char val-start-pos)
-		(when (looking-at "value is") (replace-match ""))
-		(save-excursion
-		  (insert "\n\nValue:")
+                ;; Note that setting the syntax table like below
+                ;; makes forward-sexp move over a `'s' at the end
+                ;; of a symbol.
+                (set-syntax-table emacs-lisp-mode-syntax-table)
+                (goto-char val-start-pos)
+                (when (looking-at "value is") (replace-match ""))
+                (save-excursion
+                  (insert "\n\nValue:")
                   (setq-local help-button-cache (point-marker)))
-		(insert "value is shown ")
-		(insert-button "below"
-			       'action help-button-cache
-			       'follow-link t
-			       'help-echo "mouse-2, RET: show value")
-		(insert ".\n")))
+                (insert "value is shown ")
+                (insert-button "below"
+                               'action help-button-cache
+                               'follow-link t
+                               'help-echo "mouse-2, RET: show value")
+                (insert ".\n")))
             (terpri)
 
             (let* ((alias (condition-case nil
@@ -195,8 +195,8 @@ Large values are not pretty printed."
                             (documentation-property
                              alias 'variable-documentation))))
 
-	      (with-current-buffer standard-output
-		(insert (or doc "Not documented as a variable.")))
+              (with-current-buffer standard-output
+                (insert (or doc "Not documented as a variable.")))
 
               ;; Output the indented administrative bits.
               (with-current-buffer buffer
@@ -210,9 +210,9 @@ Large values are not pretty printed."
                   (while (looking-at-p "\n")
                     (delete-char 1)))))
 
-	    (with-current-buffer standard-output
-	      ;; Return the text we displayed.
-	      (buffer-string))))))))
+            (with-current-buffer standard-output
+              ;; Return the text we displayed.
+              (buffer-string))))))))
 
 
 (advice-add 'describe-variable :override #'tv/describe-variable)
