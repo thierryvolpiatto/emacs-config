74e22c24f6cbcf4093c1e5c4cd51a65ab23eb349
Thierry Volpiatto <thievol@posteo.net>
2024-09-15 16:28:44 +0200

    Fix signs in genpasswd

--- tv-utils.el
+++ tv-utils.el
@@ -397,7 +397,7 @@ the password will be of length (floor LIMIT)."
                         "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G"
                         "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R"
                         "S" "T" "U" "V" "W" "X" "Y" "Z"]
-           with signs = ["#" "!" "$" "&" "~" "-" "_" "@" "%" "*"] 
+           with signs = ["$" "@" "&" "+" "-" "/" "#" "_" "?" "!"]
            ;; Divide by 2 because collecting 2 list.
            for i from 1 to (floor (/ limit 2))
            for rand1 = (int-to-string (random 9))
