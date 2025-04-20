3fd070c42b6e909fda4ddf9f759aa51301e065a8
Thierry Volpiatto <thievol@posteo.net>
2025-04-20 08:23:27 +0200

    Fix gnus-user-date-format-alist for today

--- gnus-config.el
+++ gnus-config.el
@@ -179,7 +179,7 @@ This will run in `message-send-hook'."
     (if time (format-time-string "%b %d  %H:%M" time) "")))
 
 (setq gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f %* %B%s%)\n"
-      gnus-user-date-format-alist '(((gnus-seconds-today) . "Today, %H:%M")
+      gnus-user-date-format-alist '(((gnus-seconds-today) . "Today,     %H:%M")
                                     ((+ 86400 (gnus-seconds-today)) . "Yesterday, %H:%M")
                                     (t . "%d.%m.%Y %H:%M"))
       gnus-sum-thread-tree-false-root ""
