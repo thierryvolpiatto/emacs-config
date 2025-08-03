820a01ea107d9ea3861f8d1f6aae644632745ec4
Thierry Volpiatto <thievol@posteo.net>
2025-08-03 21:29:48 +0200

    Cleanup org config

--- org-config.el
+++ org-config.el
@@ -27,7 +27,6 @@
         ("FIXED" . ((:foreground "SpringGreen4" :weight bold)))
         ("DEFERRED"  . shadow)
         ("CANCELED"  . ((:foreground "blue" :weight bold)))))
-        
 
 (setq org-log-done 'time)
 (setq org-use-fast-todo-selection t)
@@ -44,6 +43,7 @@
                       ("vtt")
                       ("montagne")
                       ("cascade")
+                      ("ski")
                       ("github")
                       ("helm")
                       ("async")
@@ -64,29 +64,8 @@
         ("b" "Bash"   entry (file+headline  "~/org/notes.org " "Bash")    "** %^{Title}\n  %i\n  %a" :prepend t)
         ("L" "Linux"  entry (file+headline  "~/org/notes.org"  "Linux")   "** %^{Title}\n  %i\n  %a" :prepend t)))
 
-;; Org-protocol
-;; See https://orgmode.org/worg/org-contrib/org-protocol.html for
-;; setting it up.
-;; (require 'org-protocol)
-
-;; org-annotation-helper 
-;; (require 'org-annotation-helper)
-
 ;; Diary-integration-in-org 
-(setq org-agenda-include-diary t) ; show also content of regular diary file.
-;; (setq org-agenda-diary-file "~/org/diary.org")
-
-;; Subtasks 
-(defun org-summary-todo (n-done n-not-done)
-  "Switch entry to DONE when all subentries are done, to TODO otherwise."
-  (let (org-log-done org-log-states)   ; turn off logging
-    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
-
-(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
-
-;; leave-a-blank-line-when-insert-new-item 
-(setq org-blank-before-new-entry '((heading . t)
-                                   (plain-list-item . nil)))
+(setq org-agenda-include-diary t)
 
 (defun tv:insert-org-src-keyword ()
   (interactive)
@@ -114,44 +93,6 @@
 	  (lambda ()
 	    (define-key org-mode-map (kbd "<f11> o") 'tv:org-headings)
 	    (define-key org-mode-map (kbd "<f11> k") 'tv:insert-org-src-keyword)))
-                                             
-;; Colorize-Diary's-entries-in-agenda 
-(defvar tv:diary-regexp "^ *[Dd]iary")
-(defvar tv:diary-done-regexp "^ *[Dd]iary.* *==> *done$") 
-(defun tv:org-propertize-diary-entries ()
-  (save-excursion
-    (let ((inhibit-read-only t))
-      (goto-char (point-min))
-      (while (not (eobp))
-        (cond ((re-search-forward tv:diary-done-regexp (point-at-eol) t)
-               (add-text-properties
-                (point-at-bol) (point-at-eol) '(face 'tv:org-diary-done)))
-              ((re-search-forward tv:diary-regexp (point-at-eol) t)
-               (add-text-properties
-                (point-at-bol) (point-at-eol) '(face tv:org-diary))))
-        (forward-line 1)))))
-(add-hook 'org-finalize-agenda-hook 'tv:org-propertize-diary-entries)
-
-(defface tv:org-diary '((t (:foreground "Yellow3" :underline t)))
-  "*Face for diary entry in org agenda."
-  :group 'org)
-
-(defface tv:org-diary-done '((t (:foreground "darkgrey")))
-  "*Face for finished diary entry in org agenda."
-  :group 'org)
-
-(defun tv:org-propertize-note-entry ()
-  (save-excursion
-    (let ((inhibit-read-only t))
-      (goto-char (point-min))
-      (while (re-search-forward "^ +note" nil t)
-        (add-text-properties (match-beginning 0) (point-at-eol)
-                             '(face '((:foreground "magenta"))))))))
-(add-hook 'org-finalize-agenda-hook 'tv:org-propertize-note-entry)
-
-
-(add-hook 'org-agenda-mode-hook
-	  (lambda () (set-face-attribute 'org-agenda-date-weekend nil :foreground "red")))
 
 ;; org-crypt 
 (org-crypt-use-before-save-magic)
@@ -160,24 +101,10 @@
 (define-key org-mode-map (kbd "C-c e") 'org-encrypt-entry)
 (define-key org-mode-map (kbd "C-c d") 'org-decrypt-entry)
 
-;; fontify source code
-(setq org-src-fontify-natively t)
-
 ;; Always show full path of files 
 (setq org-link-file-path-type 'absolute)
 
-;; Invisible edits
-(setq org-catch-invisible-edits 'smart)
-
 (setq org-show-context-detail '((default . local)))
-
-;; Org babel
-(with-eval-after-load 'ob-emacs-lisp
-  ;; active Babel languages
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((gnuplot . t)
-     (emacs-lisp . t))))
 
 (define-key org-mode-map (kbd "<M-up>")     'tv:scroll-up)
 (define-key org-mode-map (kbd "<M-down>")   'tv:scroll-down)
@@ -202,10 +129,6 @@
     (advice-add 'org-persist-clear-storage-maybe :around #'zz/advice--org-persist)
     (when org-persist--refresh-gc-lock-timer
       (cancel-timer org-persist--refresh-gc-lock-timer))))
-
-;; Imenu
-(setq org-imenu-depth 4)
-
 
 (provide 'org-config)
 
