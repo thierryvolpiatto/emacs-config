521e2c59ed3136aec8f2ee0bb84383604987ea45
Thierry Volpiatto <thievol@posteo.net>
2026-05-05 07:57:35 +0200

    Remove get-free-disk-space advice for emacs-26

--- dired-extension.el
+++ dired-extension.el
@@ -108,45 +108,6 @@
    (list (concat dired-re-maybe-mark dired-re-inode-size dired-re-perms ".*:$")
 	 '(".+" (dired-move-to-filename) nil (0 'default)))))
 
-
-;;; showup size available when -h arg of ls used.
-
-(defun tv:-advice-get-free-disk-space (dir)
-  (unless (file-remote-p (expand-file-name dir))
-    (save-match-data
-      ;; That is for windows.
-      (if (fboundp 'file-system-info)
-	  (let ((fsinfo (file-system-info dir)))
-	    (if fsinfo
-	        (format "%.0f" (/ (nth 2 fsinfo) 1024))))
-        ;; And this is for Linux.
-        (when (executable-find directory-free-space-program)
-          (cl-getf (tv:get-disk-info dir 'human) :available))))))
-
-(defun tv:get-disk-info (directory &optional human)
-  (let* ((directory-free-space-args
-          (if (and dired-actual-switches
-                   (string-match "h" dired-actual-switches))
-              (concat directory-free-space-args "h")
-            directory-free-space-args))
-         (default-directory (expand-file-name directory))
-         (dir  (or (file-remote-p default-directory 'localname)
-                   default-directory))
-         (args (if human
-                   (concat directory-free-space-args "h")
-                 directory-free-space-args))
-         (data (with-temp-buffer
-                 (process-file directory-free-space-program
-                               nil t nil args dir)
-                 (split-string (buffer-string) "\n" t)))
-         (values (split-string (cadr data))))
-    (cl-loop for i in '(:device :blocks :used :available :capacity :mount-point)
-             for j in values
-             append (list i j))))
-
-(when (< emacs-major-version 27)
-  (advice-add 'get-free-disk-space :override 'tv:-advice-get-free-disk-space))
-
 
 (provide 'dired-extension)
 
